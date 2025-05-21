92836c9990c00c66d255e3f1abb7178a5a80caaa
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2025-05-21 23:57:04 +0200

    faq

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,9 +1,4 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import {Accordion,AccordionContent,AccordionItem,AccordionTrigger,} from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
